cbea507353c45dd2c11fb85b8fff1bf63197033f
Taylor Jones <monitorjbl@gmail.com>
2015-07-23 23:09:12 -0400

    adding warning to config screen when a user sets required reviews to be greater than the number of required reviewers (#8)

--- src/main/resources/config.js
+++ src/main/resources/config.js
@@ -1,59 +1,69 @@
 (function ($) {
-    var baseUrl, projectKey, repoSlug;
+  var baseUrl, projectKey, repoSlug;
 
-console.log("asdfasfasdfasdf");
-    function saveConfig() {
-        $.ajax({
-            url: baseUrl + "/rest/pr-harmony/1.0/config/" + projectKey + "/" + repoSlug,
-            type: "PUT",
-            contentType: "application/json",
-            data: JSON.stringify({
-                requiredReviews: $('#requiredReviews').val(),
-                requiredReviewers: $('#requiredReviewers').val().split(','),
-                requiredReviewerGroups: $('#requiredReviewerGroups').val().split(','),
-                defaultReviewers: $('#defaultReviewers').val().split(','),
-                defaultReviewerGroups: $('#defaultReviewerGroups').val().split(','),
-                excludedUsers: $('#excludedUsers').val().split(','),
-                excludedGroups: $('#excludedGroups').val().split(','),
-                blockedCommits: $('#blockedCommits').val().split(','),
-                blockedPRs: $('#blockedPRs').val().split(','),
-                automergePRs: $('#automergePRs').val().split(','),
-                automergePRsFrom: $('#automergePRsFrom').val().split(',')
-            }),
-            success: function (config) {
-                location.reload();
-            }
-        });
+  function saveConfig() {
+    var requiredReviews = parseInt($('#requiredReviews').val());
+    var requiredReviewers = $.grep($('#requiredReviewers').val().split(','), function (v) {return v != ''});
+    var requiredReviewerGroups = $.grep($('#requiredReviewerGroups').val().split(','), function (v) {return v != ''});
+
+    if (requiredReviews > requiredReviewers.length && requiredReviewerGroups.length == 0) {
+      alert(
+          "You've specified a number of required reviews greater than the number of required reviewers. " +
+          "This will make PRs impossible to merge, please add some required reviewers.");
+      return;
     }
 
-    function getConfig() {
-        $.ajax({
-            url: baseUrl + "/rest/pr-harmony/1.0/config/" + projectKey + "/" + repoSlug,
-            dataType: "json",
-            success: function (config) {
-                $('#requiredReviews').val(config.requiredReviews);
-                $('#requiredReviewers').val(config.requiredReviewers);
-                $('#requiredReviewerGroups').val(config.requiredReviewerGroups);
-                $('#defaultReviewers').val(config.defaultReviewers);
-                $('#defaultReviewerGroups').val(config.defaultReviewerGroups);
-                $('#excludedUsers').val(config.excludedUsers);
-                $('#excludedGroups').val(config.excludedGroups);
-                $('#blockedCommits').val(config.blockedCommits);
-                $('#blockedPRs').val(config.blockedPRs);
-                $('#automergePRs').val(config.automergePRs);
-                $('#automergePRsFrom').val(config.automergePRsFrom);
-            }
-        });
-    }
-
-    $(document).ready(function () {
-        baseUrl = $("#baseUrl").val();
-        projectKey = $("#projectKey").val();
-        repoSlug = $("#repoSlug").val();
-        $('#saveButton').click(function () {
-            saveConfig();
-        });
-        getConfig();
+    $.ajax({
+      url: baseUrl + "/rest/pr-harmony/1.0/config/" + projectKey + "/" + repoSlug,
+      type: "PUT",
+      contentType: "application/json",
+      data: JSON.stringify({
+        requiredReviews: requiredReviews,
+        requiredReviewers: requiredReviewers,
+        requiredReviewerGroups: requiredReviewerGroups,
+        defaultReviewers: $('#defaultReviewers').val().split(','),
+        defaultReviewerGroups: $('#defaultReviewerGroups').val().split(','),
+        excludedUsers: $('#excludedUsers').val().split(','),
+        excludedGroups: $('#excludedGroups').val().split(','),
+        blockedCommits: $('#blockedCommits').val().split(','),
+        blockedPRs: $('#blockedPRs').val().split(','),
+        automergePRs: $('#automergePRs').val().split(','),
+        automergePRsFrom: $('#automergePRsFrom').val().split(',')
+      }),
+      success: function (config) {
+        location.reload();
+      }
     });
+  }
+
+  function getConfig() {
+    $.ajax({
+      url: baseUrl + "/rest/pr-harmony/1.0/config/" + projectKey + "/" + repoSlug,
+      dataType: "json",
+      success: function (config) {
+        $('#requiredReviews').val(config.requiredReviews);
+        $('#requiredReviewers').val(config.requiredReviewers);
+        $('#requiredReviewerGroups').val(config.requiredReviewerGroups);
+        $('#defaultReviewers').val(config.defaultReviewers);
+        $('#defaultReviewerGroups').val(config.defaultReviewerGroups);
+        $('#excludedUsers').val(config.excludedUsers);
+        $('#excludedGroups').val(config.excludedGroups);
+        $('#blockedCommits').val(config.blockedCommits);
+        $('#blockedPRs').val(config.blockedPRs);
+        $('#automergePRs').val(config.automergePRs);
+        $('#automergePRsFrom').val(config.automergePRsFrom);
+      }
+    });
+  }
+
+  $(document).ready(function () {
+    baseUrl = $("#baseUrl").val();
+    projectKey = $("#projectKey").val();
+    repoSlug = $("#repoSlug").val();
+    $('#saveButton').click(function () {
+      saveConfig();
+    });
+    getConfig();
+  });
 })(AJS.$);
 
